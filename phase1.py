bc5367e03e6e5518a9600694bdfe98c5aa549834
CHAIMAMOHSNI <CHMOH6@ulaval.ca>
2023-11-15 13:46:36 -0500

    Added h1 tag 1ér commit

--- phase1.py
+++ phase1.py
@@ -3,14 +3,13 @@ from datetime import date
 
 
 def analyser_commande():
-
-
-    parser = argparse.ArgumentParser("Extraction de valeurs historiques pour un ou plusieurs symboles boursiers.")
-    parser.add_argument('-h --help', action="help", help='show this help message and exit')
-    parser.add_argument('-d --début', dest="date_debut", type=date, help='Date recherchée la plus ancienne (format: AAAA-MM-JJ)')
-    parser.add_argument('-f --fin', dest="date_fin", type=date, help='Date recherchée la plus ancienne (format: AAAA-MM-JJ)')
+    parser = argparse.ArgumentParser(description = "Extraction de valeurs historiques pour un ou plusieurs symboles boursiers.")
+    parser.add_argument('-h ', action="help", help='show this help message and exit')
+    parser.add_argument('-d ', dest="date_debut", type=date, help='Date recherchée la plus ancienne (format: AAAA-MM-JJ)')
+    parser.add_argument('-f ', dest="date_fin", type=date, help='Date recherchée la plus ancienne (format: AAAA-MM-JJ)')
     parser.add_argument('symbole', metavar="symbole", type=str, help="Nom d'un symbole boursier")
     parser.add_argument('-v --valeur', metavar="valeur", choices=["fermeture", "ouverture", "min", "max", "volume"], default="fermeture", help="Nom d'un symbole boursier")
     args = parse.parse_args()
     return(parser.parse_args())
+
 
