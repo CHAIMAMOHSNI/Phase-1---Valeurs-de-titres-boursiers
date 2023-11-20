bd08a2c08c439eb3a3601d9b5b694472e22bdcdb
CHAIMAMOHSNI <CHMOH6@ulaval.ca>
2023-11-20 12:16:48 -0500

    presque fini

--- phase1.py
+++ phase1.py
@@ -1,22 +1,25 @@
 #projet 1 : valeur des titres boursières :
 
 import argparse
-import requests
-import datetime
-from datetime import date
+from datetime import datetime, date
 import json
+import requests
+
 
 # d'après l'enoncé on a supposé que la date existe déjà (pas de date future donnée par l'utilisateur ) + je dois corriger les défauts de PEP8 (lignes trop longues)
 def analyser_commande():
+    # la fonction suivante permet de prendre en consideration des données ( qui seront donnés par l'utlisateur) 
+    # retourne des résultats (données historiques de marché boursier pour un ou plusieurs symboles boursiers.)
+
     parser = argparse.ArgumentParser(
         description = "Extraction de valeurs historiques pour un ou plusieurs symboles boursiers."
-    )
+        )
 
     parser.add_argument(
         '-d', '--début',
         metavar='DATE' ,
-        dest= "début",
-        type=date,
+        dest= "debut",
+        type=str,
         default= "fin",
         help='Date recherchée la plus ancienne (format: AAAA-MM-JJ)',
     )
@@ -24,10 +27,10 @@ def analyser_commande():
     parser.add_argument(
         '-f', '--fin',
         metavar='DATE' ,
-        dest= "fin",
-        type=date, 
-        default= date.today() , 
-        help='Date recherchée la plus ancienne (format: AAAA-MM-JJ)'
+        dest= "datefin",
+        type=str, 
+        default= str(date.today()), 
+        help='Date recherchée la plus récente (format: AAAA-MM-JJ)'
     )
 
     parser.add_argument(
@@ -41,29 +44,36 @@ def analyser_commande():
         dest='valeur',
         choices=["fermeture", "ouverture", "min", "max", "volume"],
         default="fermeture",
-        help="Nom d'un symbole boursier",
+        help="la valeur désirée(par défaut : fermeture)",
         )
     return parser.parse_args()
 
-def produire_historique(symbole, début, fin, valeur):
+def produire_historique(symbole, debut: date, datefin: date, valeur):
+    # la fonction permet de produire l'historique complet d'une valeur boursière à partir d'un ou plusieurs symboles , la date ...
+
     url = f'https://pax.ulaval.ca/action/{symbole}/historique/'
     liste = []
 
-    params1 = {'début': début, 'fin': fin}
+    params1 = {'début': debut.strftime("%Y-%m-%d"), 'fin': datefin.strftime("%Y-%m-%d")}
     réponse = requests.get(url=url, params=params1)
     dic = json.loads(réponse.text)
     historique = dic['historique']
+
     print(f'titre ={symbole}: valeur={valeur}, début={début}, fin={fin}')
-    for dates, vals in historique.keys():
-        msg = datetime.strptime(dates, '%Y-%m-%d').date(), vals.get(valeur,'message d\'erreur')
-        liste.append(msg)  
-    return liste
+    for dates, vals in historique.items():
+        msg = datetime.strptime(ates, '%Y-%m-%d').date(), vals[valeur]
+        liste.append(msg) 
+    print(reversed(liste)) #puisque la liste sort en ordre décroissant 
 
 #Programme principal:
 
-analyse = analyser_commande()
-result = []
-for symbole in analyse.symbole:
-    données = produire_historique(symbole, analyse.début, analyse.fin, analyse.valeur)
-    result.append((symbole, données))
- 
+if __name__ == "__main__":
+    analyse = analyser_commande()
+    result = []
+    if analyse.debut is None:
+        analyse.debut = analyse.datefin
+    
+    for symb in analyse.symbole:
+        données = produire_historique(symbole, analyse.début, analyse.fin, analyse.valeur)
+        result.append((date, données))
+    
